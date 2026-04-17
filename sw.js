fd8f1b360bf6dfad8c9172ec4c6ef5d2c75b1c79
rajarshiaich <rajarshi.aich@gmail.com>
2026-04-18 00:47:02 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'ongc-audit-v6';
+const ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => Promise.allSettled(ASSETS.map(a => c.add(a))))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res && res.status === 200) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
